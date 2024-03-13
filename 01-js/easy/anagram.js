5afabeeecb7d05d0f06ddb6cebef355943a8695b
lokeshiiith <lokesh.sharma@students.iiit.ac.in>
2024-03-13 23:03:44 +0530

    add prog

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,23 @@
 */
 
 function isAnagram(str1, str2) {
+  // Check if both strings are of same length
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
+  // Make array for both strings and fill it with 0
+  let arr1 = Array(256).fill(0);
+  let arr2 = Array(256).fill(0);
+
+  for (let i = 0; i < str1.length; i++) {
+    arr1[str1.charCodeAt(i)]++;
+    arr2[str2.charCodeAt(i)]++;
+  }
+  if(arr1.toString() !== arr2.toString()) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
